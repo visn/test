a8a73b41d84c6be32576d4f5ec8db70954c329ba
visn <1831394253@qq.com>
2017-03-02 23:20:26 +0800

    visn

--- test.c
+++ test.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 int main(){
-	printf("helloworld");
+	int point = 0;
+	int input;
+	int array[10]={0,0,0,0,0,0,0,0,0,0};
+	for(;;point++){
+	scanf("%d",&input);
+	if(point>=10){point-=10;}
+        array[point]=input;
+	int i;
+		for(i=0;i<10;i++){
+		printf("list%d=%d\n",i+1,array[i]);
+		}
+	}
 	return 0;
 }
